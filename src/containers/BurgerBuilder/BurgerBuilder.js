79466472439c04d8fef7642a78ac9296a74bc0c7
rdisamir <samirp123@hotmail.co.uk>
2019-01-23 13:18:13 +0000

    Initial commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 0.7,
+    bacon: 1
+}
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients : {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
+    };
+
+    changeIngredientsHandler = (ingredientType, changeType) => {
+        let ingredientTypeCount = this.state.ingredients[ingredientType];
+        const ingredientPrice = INGREDIENT_PRICES[ingredientType];
+        let price = this.state.totalPrice;
+        if (changeType == "More") {
+            ingredientTypeCount += 1;
+            price += ingredientPrice;
+        } else {
+            if (ingredientTypeCount <= 0) {
+                return;
+            }
+            ingredientTypeCount -= 1;
+            price -= ingredientPrice;
+        }
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[ingredientType] = ingredientTypeCount;
+
+        this.setState({totalPrice: price, ingredients: updatedIngredients});
+    }
+
+    render() {
+        const disableInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disableInfo) {
+            disableInfo[key] = disableInfo[key] <= 0;
+        }
+        return (
+        <>
+            <Burger ingredients={this.state.ingredients}></Burger>
+            <div><BuildControls 
+                ingredientChanged={this.changeIngredientsHandler}
+                disabled={disableInfo}
+                price={this.state.totalPrice}
+                ></BuildControls></div>
+        </>
+        );
+    }
+}
+
+export default BurgerBuilder;
