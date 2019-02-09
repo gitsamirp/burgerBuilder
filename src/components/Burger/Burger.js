cac3e8b554093a8a092cf2137fde399b06dd9d2a
rdisamir <samirp123@hotmail.co.uk>
2019-02-09 18:25:34 +0000

    remove reduce empty array issue

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,16 +3,21 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
-    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-            return <BurgerIngredient key={igKey+i} type={igKey}></BurgerIngredient>
-        });
-    }).reduce((arr, el) => {
-        return arr.concat(el);
-    });
-    if (transformedIngredients.length === 0) {
+    console.log(props.ingredients);
+    console.log(props.ingredients.length);
+    let transformedIngredients = null;
+    if (props.ingredients.length == 0) {
         transformedIngredients = "Please add Ingredients";
+    } else {
+        transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey+i} type={igKey}></BurgerIngredient>
+            });
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        });
     }
+    
     return (
     <div className={classes.Burger}>
         <BurgerIngredient type="bread-top"></BurgerIngredient>
