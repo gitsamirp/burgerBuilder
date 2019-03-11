e58f5cdebdd7fddcc8893b62287893de6f21946c
rdisamir <samirp123@hotmail.co.uk>
2019-03-11 21:45:28 +0000

    get orders by user
    requires update to firebase rules to add userId as a index

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -106,7 +106,8 @@ class ContactData extends Component {
         const order = {
             ingredients: {...this.props.ings},
             price: this.props.price,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
         }
         console.log(order);
         this.props.onPurchaseOrder(order, this.props.token);
@@ -191,7 +192,8 @@ const mapStateToProps = state => {
         ings: state.burger.ingredients,
         price: state.burger.totalPrice,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 
