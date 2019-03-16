4eaa1aa5edcd0cd83a504760bd184f9e921b648d
rdisamir <samirp123@hotmail.co.uk>
2019-03-16 13:38:46 +0000

    share checkvalidity

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,7 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 import { connect } from 'react-redux';
 import * as orderActions from '../../../store/actions/index';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
     state = {
@@ -114,26 +114,11 @@ class ContactData extends Component {
         this.props.onPurchaseOrder(order, this.props.token);
     }
 
-    checkValidity ( value, rules ) {
-        let isValid = true;
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-        return isValid;
-    }
-
     inputChangedHandler = (event, inputIdentifier) => {
 
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
             touched:true
         });
         const updatedOrderForm = updateObject(this.state.orderForm, {
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -3,4 +3,24 @@ export const updateObject = (oldObject, updatedPropeties) => {
         ...oldObject,
         ...updatedPropeties
     }
+};
+
+export const checkValidity = ( value, rules ) => {
+    let isValid = true;
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+        const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        isValid = pattern.test(value) && isValid
+    }
+    return isValid;
 }
